aec2d7884ee9978ef929f29d81a61503bad3b70e
Alex Kocharin <alex@kocharin.ru>
2019-03-28 20:33:54 +0300

    Improve test coverage

--- test/test_cli.js
+++ test/test_cli.js
@@ -2,12 +2,15 @@
 
 
 const assert            = require('assert');
-const { join }          = require('path');
 const { execFileSync }  = require('child_process');
+const fs                = require('fs');
+const path              = require('path');
+const rimraf            = require('rimraf');
 const { run }           = require('../lib/cli');
 
 
-const script_path = join(__dirname, '../lv_font_conv.js');
+const script_path = path.join(__dirname, '../lv_font_conv.js');
+const font = require.resolve('roboto-fontface/fonts/roboto/Roboto-Black.woff');
 
 
 describe('Script', function () {
@@ -29,5 +32,40 @@ describe('Script', function () {
     assert.throws(() => {
       run('--font test --range invalid'.split(' '), true);
     }, /Invalid range value/);
+  });
+
+
+  it('Should print error if size is invalid', function () {
+    assert.throws(() => {
+      run('-s 10xxx'.split(' '), true);
+    }, /Invalid int value/);
+  });
+
+
+  it('Should print error if size is zero', function () {
+    assert.throws(() => {
+      run('-s 0'.split(' '), true);
+    }, /Invalid int value/);
+  });
+
+
+  it('Should process a font', function () {
+    let rnd = Math.random().toString(16).slice(2, 10);
+    let dir = path.join(__dirname, rnd);
+
+    try {
+      run([ '--font', font, '--range', '0x20-0x22', '-s', '18', '-o', dir, '-b', '2' ], true);
+
+      assert.deepEqual(fs.readdirSync(dir), [ '20.png', '21.png', '22.png' ]);
+    } finally {
+      rimraf.sync(dir);
+    }
+  });
+
+
+  it('Should require output for "dump" writer', function () {
+    assert.throws(() => {
+      run([ '--font', font, '--range', '0x20-0x22', '-s', '18', '-b', '2' ], true);
+    }, /Output is required for/);
   });
 });
--- test/test_collect_font_data.js
+++ test/test_collect_font_data.js
@@ -2,27 +2,52 @@
 
 
 const assert            = require('assert');
-const path              = require('path');
 const collect_font_data = require('../lib/collect_font_data');
 
-const font = path.join(require.resolve('roboto-fontface'), '../../../fonts/roboto/Roboto-Black.woff');
+const font = require.resolve('roboto-fontface/fonts/roboto/Roboto-Black.woff');
 
 
 describe('Collect font data', function () {
+
+  it('Should convert range to bitmap', function () {
+    let out = collect_font_data({
+      font: [ {
+        source: font,
+        ranges: [ { range: [ 0x41, 0x42, 0x80 ] } ]
+      } ],
+      size: 18
+    });
+
+    assert.equal(out.length, 2);
+
+    assert.equal(out[0].code, 0x80);
+    assert.equal(out[0].pixels.length, 18); // height
+    assert.equal(out[0].pixels[0].length, 10); // width
+
+    assert.equal(out[1].code, 0x81);
+    assert.equal(out[1].pixels.length, 18); // height
+    assert.equal(out[1].pixels[0].length, 10); // width
+  });
+
 
   it('Should convert symbols to bitmap', function () {
     let out = collect_font_data({
       font: [ {
         source: font,
-        ranges: [ [ 'range', [ 0x41, 0x41, 0x41 ] ] ]
+        ranges: [ { symbols: 'AB' } ]
       } ],
       size: 18
     });
 
-    assert.equal(out.length, 1);
+    assert.equal(out.length, 2);
+
     assert.equal(out[0].code, 0x41);
     assert.equal(out[0].pixels.length, 18); // height
     assert.equal(out[0].pixels[0].length, 10); // width
+
+    assert.equal(out[1].code, 0x42);
+    assert.equal(out[1].pixels.length, 18); // height
+    assert.equal(out[1].pixels[0].length, 10); // width
   });
 
 
@@ -30,12 +55,44 @@ describe('Collect font data', function () {
     let out = collect_font_data({
       font: [ {
         source: font,
-        ranges: [ [ 'range', [ 0x300, 0x300, 0x300 ] ] ]
+        ranges: [ { range: [ 0x300, 0x300, 0x300 ] } ]
       } ],
       size: 18
     });
 
     // ignore those characters for now
     assert.equal(out.length, 0);
+  });
+
+
+  it('Should allow specifying same font multiple times', function () {
+    let out = collect_font_data({
+      font: [ {
+        source: font,
+        ranges: [ { range: [ 0x41, 0x41, 0x41 ] } ]
+      }, {
+        source: font,
+        ranges: [ { range: [ 0x51, 0x51, 0x51 ] } ]
+      } ],
+      size: 18
+    });
+
+    assert.equal(out.length, 2);
+  });
+
+
+  it('Should work with sparse ranges', function () {
+    let out = collect_font_data({
+      font: [ {
+        source: font,
+        ranges: [ { range: [ 0x3d0, 0x3d8, 0x3d0 ] } ]
+      } ],
+      size: 10
+    });
+
+    assert.equal(out.length, 3);
+    assert.equal(out[0].code, 0x3d1);
+    assert.equal(out[0].code, 0x3d2);
+    assert.equal(out[0].code, 0x3d6);
   });
 });
