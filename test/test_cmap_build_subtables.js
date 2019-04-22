fe3cea8db22aabe61283edec36a24ef8d6fdbc70
Alex Kocharin <alex@kocharin.ru>
2019-04-22 19:32:44 +0300

    Add a function to calculate optimal cmap tables

---
+++ test/test_cmap_build_subtables.js
@@ -0,0 +1,52 @@
+'use strict';
+
+
+const assert     = require('assert');
+const cmap_split = require('../lib/font/cmap_build_subtables');
+
+function range(from, to) {
+  return Array(to - from + 1).fill(0).map((_, i) => from + i);
+}
+
+
+describe('font/cmap_build_subtables', function () {
+
+  it('Should represent a single character as format0', function () {
+    assert.deepEqual(cmap_split([ 42 ]), [ [ 'format0', [ 42 ] ] ]);
+  });
+
+  it('Should represent two characters as spatial', function () {
+    assert.deepEqual(cmap_split([ 10, 100 ]), [ [ 'spatial', [ 10, 100 ] ] ]);
+  });
+
+  it('Should split ranges', function () {
+    assert.deepEqual(cmap_split([ 1, ...range(100, 110), 200 ]), [
+      [ 'format0', [ 1 ] ],
+      [ 'format0', range(100, 110) ],
+      [ 'format0', [ 200 ] ]
+    ]);
+  });
+
+  it('Should split more than 256 characters into multiple ranges', function () {
+    assert.deepEqual(cmap_split(range(1, 257)), [
+      [ 'format0', [ 1 ] ],
+      [ 'format0', range(2, 257) ]
+    ]);
+  });
+
+  it('Should split en+de+ru set optimally', function () {
+    let set = [
+      ...range(65, 90), ...range(97, 122), // en + de
+      196, 214, 220, 223, 228, 246, 252,   // de, umlauts and eszett
+      1025, ...range(1040, 1103), 1105,    // ru
+      7838                                 // de, capital eszett
+    ];
+
+    assert.deepEqual(cmap_split(set), [
+      [ 'format0', [ ...range(65, 90), ...range(97, 122) ] ],
+      [ 'spatial', [ 196, 214, 220, 223, 228, 246, 252, 1025 ] ],
+      [ 'format0', [ ...range(1040, 1103), 1105 ] ],
+      [ 'format0', [ 7838 ] ]
+    ]);
+  });
+});
