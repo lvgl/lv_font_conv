c8c142cb6289dad570743d3d0cd69a660b7ff693
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-06-02 11:21:17 +0200

    lvgl writer and web updates

--- web/index.js
+++ web/index.js
@@ -1,35 +1,53 @@
-import './index.scss';
-import 'bootstrap';
-
 const convert = require('../lib/convert');
 
-let selectedFile = null;
+let selectedFile1 = null;
+let selectedFile2 = null;
 
-document.querySelector('#sourceFontFile').addEventListener('change', function handleFiles() {
-  const fileList = this.files;
+function handleFiles1() {
+  const fileList1 = this.files;
 
-  if (!fileList.length) {
-    selectedFile = null;
+  if (!fileList1.length) {
+    selectedFile1 = null;
     return;
   }
 
-  const file = fileList[0];
+  const file1 = fileList1[0];
 
-  const reader = new FileReader();
-  reader.onload = e => {
-    selectedFile = {
-      name: file.name,
-      size: file.size,
-      type: file.type,
+  const reader1 = new FileReader();
+  reader1.onload = e => {
+    selectedFile1 = {
+      name: file1.name,
+      size: file1.size,
+      type: file1.type,
       data: e.target.result
     };
   };
-  reader.readAsArrayBuffer(file);
+  reader1.readAsArrayBuffer(file1);
+}
 
-}, false);
+function handleFiles2() {
+  const fileList2 = this.files;
+
+  if (!fileList2.length) {
+    selectedFile2 = null;
+    return;
+  }
+
+  const file2 = fileList2[0];
+
+  const reader2 = new FileReader();
+  reader2.onload = e => {
+    selectedFile2 = {
+      data: e.target.result,
+      name: file2.name,
+      size: file2.size,
+      type: file2.type
+    };
+  };
+  reader2.readAsArrayBuffer(file2);
+}
 
 
-// helper
 function createCanvas(width, height) {
   const canvas = document.createElement('canvas');
   canvas.width = width;
@@ -37,29 +55,84 @@ function createCanvas(width, height) {
   return canvas;
 }
 
+document.querySelector('#font_file_1').addEventListener('change', handleFiles1, false);
+document.querySelector('#font_file_2').addEventListener('change', handleFiles2, false);
 
 document.querySelector('#converterForm').addEventListener('submit', function handleSubmit(e) {
   e.preventDefault();
 
-  /*eslint-disable no-console*/
-  console.log(selectedFile);
+  var _name = document.getElementById('name').value;
+  var _size = document.getElementById('height').value;
+  var _bpp = document.getElementById('bpp').value;
+  /* eslint-disable max-depth, radix */
+  let fcnt = 0;
+  let fonts = [];
+  let r_str = document.getElementById('range_1').value;
+  let syms = document.getElementById('symbols_1').value;
+  if (selectedFile1 && (r_str.length || syms.length)) {
+    fonts[fcnt] = {
+      source_path: selectedFile1.name,
+      source_bin: selectedFile1.data,
+      ranges: [ { range : [], symbols:'' } ] };
+    fonts[fcnt].ranges[0].symbols = syms;
+    let r_sub = r_str.split(',');
+    if (r_str.length) {
+      // Parse the ranges. A range is array with 3 elements:
+      //[range start, range end, range remap start]
+      // Multiple ranges just means
+      //an other 3 element in the array
+      for (let i = 0; i < r_sub.length; i++) {
+        let r = r_sub[i].split('-');
+        fonts[fcnt].ranges[0].range[i * 3 + 0] = parseInt(r[0]);
+        if (r[1]) {
+          fonts[fcnt].ranges[0].range[i * 3 + 1] = parseInt(r[1]);
+        } else {
+          fonts[fcnt].ranges[0].range[i * 3 + 1] = parseInt(r[0]);
+        }
+        fonts[fcnt].ranges[0].range[i * 3 + 2] = parseInt(r[0]);
+      }
+    }
+    fcnt++;
+  }
+
+  r_str = document.getElementById('range_2').value;
+  syms = document.getElementById('symbols_2').value;
+  if (selectedFile2 && (r_str.length || syms.length)) {
+    fonts[fcnt] = {
+      source_path: selectedFile2.name,
+      source_bin: selectedFile2.data,
+      ranges: [ { range : [], symbols:'' } ] };
+    fonts[fcnt].ranges[0].symbols = syms;
+    if (r_str.length) {
+      let r_sub = r_str.split(',');
+      for (let i = 0; i < r_sub.length; i++) {
+        let r = r_sub[i].split('-');
+        fonts[fcnt].ranges[0].range[i * 3 + 0] = parseInt(r[0]);
+        if (r[1]) {
+          fonts[fcnt].ranges[0].range[i * 3 + 1] = parseInt(r[1]);
+        } else {
+          fonts[fcnt].ranges[0].range[i * 3 + 1] = parseInt(r[0]);
+        }
+        fonts[fcnt].ranges[0].range[i * 3 + 2] = parseInt(r[0]);
+      }
+    }
+  }
 
   const result = convert({
-    font: [
-      {
-        source_path: selectedFile.name,
-        source_bin: selectedFile.data,
-        ranges: [
-          { range: [ 0x20, 0x7f, 0x20 ] }
-        ]
-      }
-    ],
-    size: 16,
-    bpp: 4,
-    format: 'dump',
-    output: './'
+    font: fonts,
+    size: parseInt(_size, 10),
+    bpp: parseInt(_bpp, 10),
+    no_compress : true,
+    no_prefilter : true,
+    format: 'lvgl',
+    output: _name
   }, createCanvas);
 
-  console.log(result);
+  var FileSaver = require('file-saver');
 
+  var blob = new Blob([ result[_name] ], {
+    type: 'text/plain;charset=utf-8'
+  });
+
+  FileSaver.saveAs(blob, _name + '.c');
 }, false);
