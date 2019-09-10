aef1f7e26791a3d5d035d620d0c2608edc32738d
Alex Kocharin <alex@kocharin.ru>
2019-09-10 19:02:31 +0300

    Add js api draft for calling freetype

--- lib/freetype/render.c
+++ lib/freetype/render.c
@@ -1,7 +1,22 @@
 #include <emscripten.h>
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
-EMSCRIPTEN_KEEPALIVE
-float test(float a, float b)
+static void set_js_variable(char* name, int value) {
+  char buffer[strlen(name) + 32];
+  sprintf(buffer, "Module.%s = %d;", name, value);
+  emscripten_run_script(buffer);
+}
+
+// expose constants required in js
+void init_constants()
 {
-  return a + b;
+  set_js_variable("FT_LOAD_DEFAULT",            FT_LOAD_DEFAULT);
+  set_js_variable("FT_RENDER_MODE_NORMAL",      FT_RENDER_MODE_NORMAL);
+  set_js_variable("OFFSET_FACE_GLYPH",          offsetof(FT_FaceRec, glyph));
+  set_js_variable("OFFSET_GLYPH_BITMAP_LEFT",   offsetof(FT_GlyphSlotRec, bitmap_left));
+  set_js_variable("OFFSET_GLYPH_BITMAP_TOP",    offsetof(FT_GlyphSlotRec, bitmap_top));
+  set_js_variable("OFFSET_GLYPH_BITMAP_WIDTH",  offsetof(FT_GlyphSlotRec, bitmap.width));
+  set_js_variable("OFFSET_GLYPH_BITMAP_ROWS",   offsetof(FT_GlyphSlotRec, bitmap.rows));
+  set_js_variable("OFFSET_GLYPH_BITMAP_BUFFER", offsetof(FT_GlyphSlotRec, bitmap.buffer));
 }
--- support/build.sh
+++ support/build.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
-mkdir -p ./lv_font_conf/lib/freetype/build
+mkdir -p /src/lv_font_conv/lib/freetype/build
 
-emcc --bind -o ./lv_font_conf/lib/freetype/build/ft_render.js ./lv_font_conf/lib/freetype/render.c -L/usr/local/lib -lfreetype -I/usr/local/include/freetype2 -s MODULARIZE=1
+emcc --bind \
+  -o /src/lv_font_conv/lib/freetype/build/ft_render.js \
+  /src/lv_font_conv/lib/freetype/render.c \
+  -L/usr/local/lib -lfreetype -I/usr/local/include/freetype2 \
+  -s "EXPORTED_FUNCTIONS=[\
+    '_FT_Init_FreeType',\
+    '_FT_Done_FreeType',\
+    '_FT_New_Memory_Face',\
+    '_FT_Done_Face',\
+    '_FT_Set_Char_Size',\
+    '_FT_Set_Pixel_Sizes',\
+    '_FT_Get_Char_Index',\
+    '_FT_Load_Glyph',\
+    '_FT_Render_Glyph',\
+    '_init_constants'\
+  ]"\
+  -s "EXTRA_EXPORTED_RUNTIME_METHODS=[\
+    'ccall',\
+    'getValue',\
+    'writeArrayToMemory'\
+  ]"\
+  -s MODULARIZE=1
