101ab9d3769746113a427cfd76f8c7da1809dda0
Alex Kocharin <alex@kocharin.ru>
2019-09-12 19:33:22 +0300

    Fix glyph bbox for freetype

--- lib/freetype/render.c
+++ lib/freetype/render.c
@@ -11,12 +11,14 @@ static void set_js_variable(char* name, int value) {
 // expose constants required in js
 void init_constants()
 {
-  set_js_variable("FT_LOAD_DEFAULT",            FT_LOAD_DEFAULT);
-  set_js_variable("FT_RENDER_MODE_NORMAL",      FT_RENDER_MODE_NORMAL);
-  set_js_variable("OFFSET_FACE_GLYPH",          offsetof(FT_FaceRec, glyph));
-  set_js_variable("OFFSET_GLYPH_BITMAP_LEFT",   offsetof(FT_GlyphSlotRec, bitmap_left));
-  set_js_variable("OFFSET_GLYPH_BITMAP_TOP",    offsetof(FT_GlyphSlotRec, bitmap_top));
-  set_js_variable("OFFSET_GLYPH_BITMAP_WIDTH",  offsetof(FT_GlyphSlotRec, bitmap.width));
-  set_js_variable("OFFSET_GLYPH_BITMAP_ROWS",   offsetof(FT_GlyphSlotRec, bitmap.rows));
-  set_js_variable("OFFSET_GLYPH_BITMAP_BUFFER", offsetof(FT_GlyphSlotRec, bitmap.buffer));
+  set_js_variable("FT_LOAD_DEFAULT",               FT_LOAD_DEFAULT);
+  set_js_variable("FT_RENDER_MODE_NORMAL",         FT_RENDER_MODE_NORMAL);
+  set_js_variable("OFFSET_FACE_GLYPH",             offsetof(FT_FaceRec, glyph));
+  set_js_variable("OFFSET_GLYPH_BITMAP_LEFT",      offsetof(FT_GlyphSlotRec, bitmap_left));
+  set_js_variable("OFFSET_GLYPH_BITMAP_TOP",       offsetof(FT_GlyphSlotRec, bitmap_top));
+  set_js_variable("OFFSET_GLYPH_BITMAP_WIDTH",     offsetof(FT_GlyphSlotRec, bitmap.width));
+  set_js_variable("OFFSET_GLYPH_BITMAP_ROWS",      offsetof(FT_GlyphSlotRec, bitmap.rows));
+  set_js_variable("OFFSET_GLYPH_BITMAP_ADVANCE_X", offsetof(FT_GlyphSlotRec, advance.x));
+  set_js_variable("OFFSET_GLYPH_BITMAP_ADVANCE_Y", offsetof(FT_GlyphSlotRec, advance.y));
+  set_js_variable("OFFSET_GLYPH_BITMAP_BUFFER",    offsetof(FT_GlyphSlotRec, bitmap.buffer));
 }
