0c4263140b0a08078eb13699a2e6a25ba3379f96
Vitaly Puzrin <vitaly@rcdesign.ru>
2019-09-12 23:34:22 +0300

    Force autohinting & render glyphs on load

--- lib/freetype/render.c
+++ lib/freetype/render.c
@@ -8,11 +8,20 @@ static void set_js_variable(char* name, int value) {
   emscripten_run_script(buffer);
 }
 
-// expose constants required in js
+// Expose constants, used in calls from js
 void init_constants()
 {
   set_js_variable("FT_LOAD_DEFAULT",               FT_LOAD_DEFAULT);
+  set_js_variable("FT_LOAD_NO_HINTING",            FT_LOAD_NO_HINTING);
+  set_js_variable("FT_LOAD_RENDER",                FT_LOAD_RENDER);
+  set_js_variable("FT_LOAD_FORCE_AUTOHINT",        FT_LOAD_FORCE_AUTOHINT);
+  set_js_variable("FT_LOAD_PEDANTIC",              FT_LOAD_PEDANTIC);
+  set_js_variable("FT_LOAD_MONOCHROME",            FT_LOAD_MONOCHROME);
+  set_js_variable("FT_LOAD_NO_AUTOHINT",           FT_LOAD_NO_AUTOHINT);
+
   set_js_variable("FT_RENDER_MODE_NORMAL",         FT_RENDER_MODE_NORMAL);
+  set_js_variable("FT_RENDER_MODE_MONO",           FT_RENDER_MODE_MONO);
+
   set_js_variable("OFFSET_FACE_GLYPH",             offsetof(FT_FaceRec, glyph));
   set_js_variable("OFFSET_GLYPH_BITMAP_LEFT",      offsetof(FT_GlyphSlotRec, bitmap_left));
   set_js_variable("OFFSET_GLYPH_BITMAP_TOP",       offsetof(FT_GlyphSlotRec, bitmap_top));
